8dbffa0620caa278315def41165f4bf20030735d
devbhuwan <bhuwan.upadhyay49@gmail.com>
2017-11-26 09:11:23 +0400

    FRONT-END-DESIGN

--- frontend/xcs-bpm/src/app/xcs-bpm.module.ts
+++ frontend/xcs-bpm/src/app/xcs-bpm.module.ts
@@ -2,11 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BpmPanelComponent} from './bpm-panel/bpm-panel.component';
 
+import {XcsCoreModule} from '@xcs/xcs-core';
+
 export const EXPORT_COMPONENTS = [BpmPanelComponent];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    XcsCoreModule
   ],
   declarations: [BpmPanelComponent],
   exports: EXPORT_COMPONENTS
