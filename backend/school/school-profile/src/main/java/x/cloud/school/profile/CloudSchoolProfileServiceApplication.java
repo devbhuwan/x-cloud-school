7cea2d1e14c3e13962829337ef6ed486c584a4c1
devbhuwan <bhuwan.upadhyay49@gmail.com>
2017-11-30 11:28:18 +0400

    BACK_END

---
+++ backend/school/school-profile/src/main/java/x/cloud/school/profile/CloudSchoolProfileServiceApplication.java
@@ -0,0 +1,14 @@
+package x.cloud.school.profile;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSchoolProfileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudSchoolProfileServiceApplication.class, args);
+    }
+
+}
---
+++ backend/school/school-profile/src/main/java/x/cloud/school/profile/model/School.java
@@ -0,0 +1,8 @@
+package x.cloud.school.profile.model;
+
+public class School {
+
+    private String id;
+    private String name;
+
+}
