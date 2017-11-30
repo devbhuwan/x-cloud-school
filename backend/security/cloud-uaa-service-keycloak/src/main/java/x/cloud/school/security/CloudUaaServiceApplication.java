7cd8de2cf17fde67a1f523b4f0df7153a1c0ec04
devbhuwan <bhuwan.upadhyay49@gmail.com>
2017-11-30 10:29:53 +0400

    BACK_END

--- backend/security/cloud-uaa-service-keycloak/src/main/java/x/cloud/school/security/CloudUaaServiceApplication.java
+++ backend/security/cloud-uaa-service-keycloak/src/main/java/x/cloud/school/security/CloudUaaServiceApplication.java
@@ -1,9 +1,7 @@
 package x.cloud.school.security;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class CloudUaaServiceApplication {
 
     public static void main(String[] args) {
