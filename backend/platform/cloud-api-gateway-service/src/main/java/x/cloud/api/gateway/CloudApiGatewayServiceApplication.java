3153e21c5af43cab9835fe2b0cc8a1e535b1e673
devbhuwan <bhuwan.upadhyay49@gmail.com>
2017-12-02 00:42:30 +0400

    BACKEND-X-CLOUD-SCHOOL

--- backend/platform/cloud-api-gateway-service/src/main/java/x/cloud/api/gateway/CloudApiGatewayServiceApplication.java
+++ backend/platform/cloud-api-gateway-service/src/main/java/x/cloud/api/gateway/CloudApiGatewayServiceApplication.java
@@ -3,9 +3,7 @@ package x.cloud.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
 public class CloudApiGatewayServiceApplication {
 
