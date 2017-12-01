1c0318862c7de40cb88256a8bd1037a3db72f8e5
devbhuwan <bhuwan.upadhyay49@gmail.com>
2017-12-02 00:28:17 +0400

    BACKEND-X-CLOUD-SCHOOL

---
+++ backend/platform/cloud-api-gateway/src/main/java/x/cloud/api/gateway/CloudApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package x.cloud.api.gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApiGatewayApplication.class, args);
+    }
+
+}
---
+++ backend/platform/cloud-api-gateway/src/test/java/x/cloud/api/gateway/CloudApiGatewayApplicationTest.java
@@ -0,0 +1,13 @@
+package x.cloud.api.gateway;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.testng.Assert.*;
+
+@DisplayName("Cloud School Profile Service Application")
+@ExtendWith(SpringExtension.class)
+public class CloudApiGatewayApplicationTest {
+
+}
