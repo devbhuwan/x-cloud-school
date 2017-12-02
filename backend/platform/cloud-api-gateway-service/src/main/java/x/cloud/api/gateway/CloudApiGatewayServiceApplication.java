ecbce0aef2126fdd85995a6d87031b2355b37e02
devbhuwan <bhuwan.upadhyay49@gmail.com>
2017-12-02 08:27:00 +0400

    BACKEND-X-CLOUD-SCHOOL

--- backend/platform/cloud-api-gateway-service/src/main/java/x/cloud/api/gateway/CloudApiGatewayServiceApplication.java
+++ backend/platform/cloud-api-gateway-service/src/main/java/x/cloud/api/gateway/CloudApiGatewayServiceApplication.java
@@ -3,8 +3,12 @@ package x.cloud.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class CloudApiGatewayServiceApplication {
 
     public static void main(String[] args) {
