b06c8202237c3f8ed1e03c72b7b9d3da87de2a7e
devbhuwan <bhuwan.upadhyay49@gmail.com>
2017-12-08 21:49:57 +0400

    BACKEND

--- backend/security/cloud-uaa-service-keycloak/src/main/java/x/cloud/school/security/CloudUaaServiceApplication.java
+++ backend/security/cloud-uaa-service-keycloak/src/main/java/x/cloud/school/security/CloudUaaServiceApplication.java
@@ -1,6 +1,33 @@
 package x.cloud.school.security;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
+@EnableConfigurationProperties(KeycloakServerProperties.class)
 public class CloudUaaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudUaaServiceApplication.class, args);
+    }
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(ServerProperties serverProperties, KeycloakServerProperties keycloakServerProperties) {
+
+        return (evt) -> {
+
+            Integer port = serverProperties.getPort();
+            String keycloakContextPath = keycloakServerProperties.getContextPath();
+
+            System.out.printf("Embedded Keycloak started: http://localhost:%s%s to use keycloak%n", port, keycloakContextPath);
+        };
+    }
 
 
 }
