c9a74ba94d97a93088cf047d86d7a6f8bb4b0186
devbhuwan <bhuwan.upadhyay49@gmail.com>
2017-12-08 15:13:07 +0400

    BACKEND-X-CLOUD-SCHOOL

---
+++ backend/security/cloud-service-security-client/src/test/java/x/cloud/school/security/CloudSecurityApplication.java
@@ -0,0 +1,12 @@
+package x.cloud.school.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudSecurityApplication.class, args);
+    }
+}
